8f7ebfa1e4901599ef9f62891c9cf5cc1ea81130
chenmic8 <chenmic8@msu.edu>
2021-12-02 13:00:24 -0500

    moved @code to code behind

---
+++ BugTracker/Pages/FetchData.razor.cs
@@ -0,0 +1,21 @@
+﻿using BugTracker.Data;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Threading.Tasks;
+
+namespace BugTracker.Pages
+{
+    public partial class FetchData
+    {
+        [Inject]
+        WeatherForecastService ForecastService { get; set; }
+
+        private WeatherForecast[] forecasts;
+
+        protected override async Task OnInitializedAsync()
+        {
+            forecasts = await ForecastService.GetForecastAsync(DateTime.Now);
+        }
+    }
+}
+
