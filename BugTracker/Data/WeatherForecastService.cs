448f951a765f15a14c297f64bf0847af20b08123
Michelle Chen <chenmic8@msu.edu>
2021-12-07 13:04:33 -0500

    Added button on modal page that allows input of Bug

--- BugTracker/Data/WeatherForecastService.cs
+++ BugTracker/Data/WeatherForecastService.cs
@@ -12,13 +12,17 @@ namespace BugTracker.Data
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
+        //create a type Task  Class (specifically WeatherForcast task class type) called GetForecastAsync that accepts a DateTime variable
         public Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
         {
             var rng = new Random();
+            //returns 5 different WeatherForecasts containing the Date, TemperatureC, and Summary to an array
             return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = startDate.AddDays(index),
+                //TemperatureC is between -20 and 55
                 TemperatureC = rng.Next(-20, 55),
+                //Summary is pulled from the above list Summaries, with the max index being the length of the list
                 Summary = Summaries[rng.Next(Summaries.Length)]
             }).ToArray());
         }
